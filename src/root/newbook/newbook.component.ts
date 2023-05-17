5164afddf3bab8f0d34e8d3de3fcbd39a9ba5a2a
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-17 18:54:04 +0200

    some changes in form

--- src/root/newbook/newbook.component.ts
+++ src/root/newbook/newbook.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-newbook',
   templateUrl: './newbook.component.html',
   styleUrls: ['./newbook.component.css'],
   imports: [CommonModule],
-  standalone: true
+  standalone: true,
 })
 export class NewbookComponent implements OnInit {
   bookForm: boolean = false;
-  addbooktext: string = "+ Aggiungi un libro";
-  constructor() { }
+  addbooktext: string = '+ Aggiungi un libro';
+  constructor() {}
 
-  ngOnInit() {
-  }
-  showForm(){
-    if (this.bookForm==false){
-    this.bookForm = true;
-    this.addbooktext = "- Nascondi";
-    }
-    else {
+  ngOnInit() {}
+  showForm() {
+    if (this.bookForm == false) {
+      this.bookForm = true;
+      this.addbooktext = '- Nascondi';
+    } else {
       this.bookForm = false;
-      this.addbooktext = "+ Aggiungi un libro";
+      this.addbooktext = '+ Aggiungi un libro';
     }
   }
-
 }
