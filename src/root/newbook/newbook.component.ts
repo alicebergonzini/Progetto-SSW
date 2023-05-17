b93e79820663791b16ef0538e3e896ef33386829
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-17 18:10:19 +0200

    form changees

--- src/root/newbook/newbook.component.ts
+++ src/root/newbook/newbook.component.ts
@@ -16,8 +16,14 @@ export class NewbookComponent implements OnInit {
   ngOnInit() {
   }
   showForm(){
+    if (this.bookForm==false){
     this.bookForm = true;
     this.addbooktext = "- Nascondi";
+    }
+    else {
+      this.bookForm = false;
+      this.addbooktext = "+ Aggiungi un libro";
+    }
   }
 
 }
