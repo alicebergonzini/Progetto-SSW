23d33cdb93f5993a9f29a997589a084b7463fc72
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-19 19:39:29 +0200

    no empty input control

--- src/root/newbook/newbook.component.ts
+++ src/root/newbook/newbook.component.ts
@@ -14,8 +14,10 @@ export class NewbookComponent implements OnInit {
   @Output() newBookEvent = new EventEmitter<Book>(); 
   bookForm: boolean = false;
   addbooktext: string = '+ Aggiungi un libro';
+  errorMsg: string = "";
   constructor() {}
   ngOnInit() {}
+  //metodo che in base al parametro bookForm, mostra o nasconde il form
   showForm() {
     if (this.bookForm == false) {
       this.bookForm = true;
@@ -25,15 +27,23 @@ export class NewbookComponent implements OnInit {
       this.addbooktext = '+ Aggiungi un libro';
     }
   }
+  //metodo che viene invocato quando il form viene "inviato";
   onSubmit()  {
+    //aggiungere controlli sui singoli campi!!!
     var title: HTMLInputElement = document.getElementById('nbtitolo') as HTMLInputElement;
     var author: HTMLInputElement = document.getElementById('nbautore') as HTMLInputElement;
     var position: HTMLInputElement = document.getElementById('nbposizione') as HTMLInputElement;
+    if(title.value == "" || author.value == "" || position.value == ""){
+      this.errorMsg = "Errore: Compila tutti i campi";
+      return;
+    }
+    this.errorMsg = "";
     var newBook: Book = new Book(title.value, author.value, position.value, undefined);
     this.newBookEvent.emit(newBook);
     title.value = "";
     author.value = "";
     position.value = "";
+    
   }
    
 }
