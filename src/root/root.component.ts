4a49f8e0015237b588ac1941e22d61b1edbf7b69
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-23 16:35:17 +0200

    number of books msg works!

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -17,6 +17,7 @@ import { ResultComponent } from './result/result.component';
 export class RootComponent implements OnInit {
   library: Library = new Library([new Book("Il Fu Mattia Pascal", "Luigi Pirandello", "P222", undefined), new Book("Harry Potter e la Pietra Filosofale", "J.K. Rowling", "U820", undefined), new Book("Harry Potter e il Calice di Fuoco", "J.K. Rowling", "N712", undefined), new Book("Lolita", "Vladimir Nabakov", "B288", undefined), new Book("Orgoglio e Pregiudizio", "Jane Austen", "D749", new User("Pippo", "Baudo", 1)), new Book("I Promessi Sposi", "Alessandro Manzoni", "L332", undefined) ]);
   booksfound: Array<Book> = [];
+  bf_count: number = 0;
   bf_message: string = "";
   
   constructor() { }
@@ -33,6 +34,8 @@ export class RootComponent implements OnInit {
      this.library.deleteBook(book);
     }
     var div2del: HTMLDivElement = document.getElementById(book.posizione) as HTMLDivElement;
+    this.bf_count += -1;
+    this.msgFound(this.bf_count);
     div2del.remove();
   }
   searchbook(searchedstring: string){
@@ -44,14 +47,17 @@ export class RootComponent implements OnInit {
           this.booksfound.push(book);
         }
       })
-      if(this.booksfound.length>1){
-        this.bf_message = this.booksfound.length + " libri trovati";
-      } else if (this.booksfound.length==1){
-        this.bf_message = this.booksfound.length + " libro trovato";
-      } else if (this.booksfound){
-        this.bf_message = "Nessun libro trovato";
-      }
-      console.log(this.booksfound);
+      this.bf_count = this.booksfound.length;
+      this.msgFound(this.bf_count);
+    }
+  }
+  msgFound(count:number){
+    if(count>1){
+      this.bf_message = count + " libri trovati";
+    } else if (count==1){
+      this.bf_message = count + " libro trovato";
+    } else if(count==0){
+      this.bf_message = "Nessun libro trovato";
     }
   }
 
