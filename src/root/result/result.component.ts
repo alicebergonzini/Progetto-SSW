ba0cb8c3ae3dce5b8c053146194172e5c001069b
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-23 12:29:39 +0200

    Delet and optional loan-return buttons added

--- src/root/result/result.component.ts
+++ src/root/result/result.component.ts
@@ -1,20 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core'; 
-import {Book} from '../classes'
+import {Book} from '../classes';
+
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule]
 })
 export class ResultComponent implements OnInit {
   //inizializzo perché mi obbliga a farlo; è corretto? 
   @Input() book_result: Book = new Book("", "", "", undefined);
   status: string = ""; 
+  isDisponibile: boolean = true;
   constructor() { }
 
   ngOnInit() {
     this.status =  this.book_result.utenteNol === undefined ? "Disponibile" : "Noleggiato";
+    this.isDisponibile =  this.book_result.utenteNol != undefined ? false: true;
   }
 
   
