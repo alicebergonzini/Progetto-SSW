27465d73ad50009db2eb451202cd5457dc949c3b
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-24 11:40:41 +0200

    noleggio component added

---
+++ src/root/noleggio/noleggio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noleggio',
+  templateUrl: './noleggio.component.html',
+  styleUrls: ['./noleggio.component.css'],
+  standalone: true
+})
+export class NoleggioComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit() {
+  }
+}
--- src/root/result/result.component.ts
+++ src/root/result/result.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core'; 
 import {Book} from '../classes';
+import { NoleggioComponent } from '../noleggio/noleggio.component';
 
 
 @Component({
@@ -8,14 +9,16 @@ import {Book} from '../classes';
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, NoleggioComponent]
 })
 export class ResultComponent implements OnInit {
   //inizializzo perché mi obbliga a farlo; è corretto? 
   @Input() book_result: Book = new Book("", "", "", undefined);
+  @Input() id: string = "";
   @Output() deleteEvent = new EventEmitter<Book>(); 
   status: string = ""; 
   isDisponibile: boolean = true;
+  noleggiando: boolean = false;
   constructor() { }
 
   ngOnInit() {
@@ -25,5 +28,9 @@ export class ResultComponent implements OnInit {
   onDelete(){
     this.deleteEvent.emit(this.book_result);
   }
-  
+  openNoleggia(){
+    this.noleggiando = true;
+    var div_result: HTMLDivElement = document.getElementById(this.id) as HTMLDivElement; 
+    div_result.style.height = "130px";
+  }
 }
