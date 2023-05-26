6f67a187010b86d9e0252270368eff84fdc7e0f0
alicebergonzini <62393907+alicebergonzini@users.noreply.github.com>
2023-05-26 12:27:33 +0200

    Research implemented with service

--- src/root/library.service.ts
+++ src/root/library.service.ts
@@ -18,7 +18,7 @@ export class LibraryService {
     });
   }
   //metodo che chiama una callback sul dato e lo immette nel server
-  setLibrary(library: Library): Observable<AjaxResponse<any>> {
+  setLibrary(library: Array<Book>): Observable<AjaxResponse<any>> {
   return ajax({
     method: 'POST',
     url: this.URL + 'set?key=' + this.apiKey,
@@ -26,26 +26,15 @@ export class LibraryService {
     body: JSON.stringify(library),
   });
   }
-  //metodo che fa la subscribe di get e chiama una callback sul dato ottenuto
-  addbook(book: Book){
-  this.getLibrary().subscribe({
-    next: (x: AjaxResponse<any>) => {
-    var library: Library = JSON.parse(x.response);
-    //perché mi dice che addbook non va bene?
-    library.books.push(book);
-    this.setSub(library);
-    },
-    error: (err) =>
-      console.error('La richiesta ha dato un errore: ' + JSON.stringify(err)),
-    });
-  }
 
   //metodo che fa la subscribe di set 
-  setSub(library: Library){
+  setSub(library: Array<Book>){
   this.setLibrary(library).subscribe({
     next: (x: AjaxResponse<any>) => {},
     error: (err) => console.error('Errore: ' + JSON.stringify(err)),
     });
+    }
   }
 
-  }
+  
+  
--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component'
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 import {ResearchComponent} from './research/research.component'
 import { NewbookComponent } from './newbook/newbook.component';
 import {Library, Book, User} from './classes';
@@ -27,10 +28,19 @@ export class RootComponent implements OnInit {
   ngOnInit() {
   }
 
-  newbook(newbook: Book){
-    //!! Attenzione, set va richiamata nella next della subscribe della get!!!!
-    this.ls.addbook(newbook);
-  }
+    //metodo che fa la subscribe di get e chiama una callback sul dato ottenuto
+  newbook(book: Book){
+    this.ls.getLibrary().subscribe({
+      next: (x: AjaxResponse<any>) => {
+      this.library.books = JSON.parse(x.response);
+      this.library.addBook(book);
+      this.ls.setSub(this.library.books);
+      },
+      error: (err) =>
+        console.error('La richiesta ha dato un errore: ' + JSON.stringify(err)),
+      });
+    }
+    
   
   removebook(book: Book){
     if(confirm("Are you sure to delete " + book.titolo)) {
@@ -40,23 +50,25 @@ export class RootComponent implements OnInit {
      this.msgFound(this.bf_count);
      div2del.remove();
     }
-   
   }
+
   searchbook(searchedstring: string){
-    this.booksfound = [];
-    if(searchedstring!=""){
-      this.library.books.forEach((book)=>{
-        var combined:string = book.titolo.toLowerCase() + book.autore.toLowerCase();
-        if(combined.includes(searchedstring.toLowerCase())){
-          this.booksfound.push(book);
-        }
-      })
-      this.bf_count = this.booksfound.length;
-      this.msgFound(this.bf_count);
-    } else {
-      this.bf_message = "";
+    this.ls.getLibrary().subscribe({
+      next: (x: AjaxResponse<any>) => {
+        this.booksfound = [];
+        this.library.books = JSON.parse(x.response);
+        if(searchedstring!=""){
+          this.booksfound = this.library.books.filter((book) => (book.titolo.toLowerCase()+book.autore.toLocaleLowerCase()).includes(searchedstring.toLocaleLowerCase())); 
+          this.bf_count = this.booksfound.length;
+          this.msgFound(this.bf_count);
+        } else {
+          this.bf_message = "";
+        } },
+        error: (err) =>
+        console.error('La richiesta ha dato un errore: ' + JSON.stringify(err)),
+      });
     }
-  }
+  
   msgFound(count:number){
     if(count>1){
       this.bf_message = count + " libri trovati";
